2665c1a0f301d0f0df8979a64c8fdfeea367dbc4
Nir Gazit <nir.gzt@gmail.com>
2023-02-16 19:03:01 -0800

    fix: jest preset references

--- packages/jest-opentelemetry/jest-preset.js
+++ packages/jest-opentelemetry/jest-preset.js
@@ -1,6 +1,6 @@
 module.exports = {
   globalSetup: require.resolve('jest-environment-otel/setup'),
   globalTeardown: require.resolve('jest-environment-otel/teardown'),
-  // testEnvironment: require.resolve('jest-environment-otel'),
-  // setupFilesAfterEnv: [require.resolve('expect-otel')],
+  testEnvironment: require.resolve('jest-environment-otel'),
+  setupFilesAfterEnv: [require.resolve('expect-opentelemetry')],
 };
