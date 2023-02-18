ecc632db4921f85ff23c3cc31317c847a050f656
Gal Kleinman <gal.klm@gmail.com>
2023-02-17 16:17:20 -0800

    wip

--- packages/expect-opentelemetry/src/utils.ts
+++ packages/expect-opentelemetry/src/utils.ts
@@ -1,4 +1,4 @@
-import { Root, load } from 'protobufjs';
+import { TracesData } from './proto';
 
 export const getInstanceType = (instance: any) => {
   if (
@@ -18,35 +18,9 @@ export const enhanceError = (error: any, message: string) => {
 
 export const parseServerResponse = (data: any) => {
   const tracesBinary = Buffer.from(data.traces, 'base64');
-
-  load('./proto/trace.proto', function (err, root) {
-    if (err) throw err;
-    if (!root) throw new Error('root is undefined');
-
-    const TracesData = root.lookupType(
-      'opentelemetry.proto.trace.v1.TracesData',
-    );
-
-    const decoded = TracesData.decode(tracesBinary);
-    console.log(`decoded = ${JSON.stringify(decoded)}`);
-  });
+  const traces = TracesData.decode(tracesBinary);
 };
 
 export const generateStubData = () => {
-  console.log('generateStubData');
-  console.log('__dirname', __dirname);
-
-  const root = new Root();
-  root.resolvePath = (origin, target) => {
-    console.log(`origin: ${origin}, target: ${target}`);
-    return `${__dirname}/proto/` + target;
-  };
-
-  root.loadSync('trace/v1/trace.proto');
-
-  const TracesData = root.lookupType('opentelemetry.proto.trace.v1.TracesData');
-
-  console.log('TracesData', TracesData);
-
-  return TracesData.create();
+  return TracesData.create({});
 };
