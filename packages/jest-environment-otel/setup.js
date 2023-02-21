57c94a448045ac00a1c01788a8439489a2ca6fe2
Nir Gazit <nir.gzt@gmail.com>
2023-02-20 23:54:11 -0800

    fix: adaptations for minimal working js project

--- packages/jest-environment-otel/setup.js
+++ packages/jest-environment-otel/setup.js
@@ -1,3 +1,4 @@
-import { setup } from './dist/global';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const setup = require('./dist/global');
 
 export default setup;
--- packages/jest-environment-otel/teardown.js
+++ packages/jest-environment-otel/teardown.js
@@ -1,3 +1,4 @@
-import { teardown } from './dist/global';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const teardown = require('./dist/global');
 
 export default teardown;
