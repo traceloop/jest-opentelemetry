30c01fe729e1fd9c446968e0aa03140743ac9681
Gal Kleinman <gal.klm@gmail.com>
2023-02-20 20:45:31 -0800

    refactor syntax

--- packages/expect-opentelemetry/src/trace.ts
+++ packages/expect-opentelemetry/src/trace.ts
@@ -3,18 +3,20 @@ import { Service } from './resources/service';
 import { opentelemetry } from '../../../proto';
 import { promiseHttpGet } from './utils';
 
-export async function traces(fn: () => Promise<void>) {
-  await fn();
-  await setTimeout(5000);
-  const t = new Trace();
-  await t.init();
-  return t;
-}
-
 export class Trace {
   private tracesData: opentelemetry.proto.trace.v1.ITracesData | undefined;
 
-  async init() {
+  static async trigger(fn: () => Promise<void>) {
+    const trace = new Trace();
+
+    await fn();
+    await setTimeout(5000);
+    await trace.capture();
+
+    return trace;
+  }
+
+  async capture() {
     const response = await promiseHttpGet('http://localhost:4123/v1/traces');
 
     this.tracesData = opentelemetry.proto.trace.v1.TracesData.decode(response);
