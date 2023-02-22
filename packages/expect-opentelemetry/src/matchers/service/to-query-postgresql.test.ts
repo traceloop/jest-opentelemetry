013d698fb07242dfb5a33e49aefd66e3a677f384
Gal Kleinman <gal.klm@gmail.com>
2023-02-21 21:45:45 -0800

    add pg statment matcher

---
+++ packages/expect-opentelemetry/src/matchers/service/to-query-postgresql.test.ts
@@ -0,0 +1,32 @@
+import { jest, describe, it } from '@jest/globals';
+import { expectTrace } from '../..';
+import { TraceLoop } from '../../trace-loop';
+import { COMPARE_TYPE } from '../utils';
+
+jest.setTimeout(30000);
+
+describe('postgresql query', () => {
+  let traceloop: TraceLoop;
+  beforeAll(async () => {
+    traceloop = new TraceLoop();
+
+    await traceloop.axiosInstance.post('http://localhost:3000/orders/create');
+    await traceloop.fetchTraces();
+  });
+
+  it('should see orders-service querying postgresql named postgres', async () => {
+    expectTrace(traceloop.serviceByName('orders-service'))
+      .toQueryPostgreSQL()
+      .withDatabaseName('postgres', { compareType: COMPARE_TYPE.EQUALS });
+  });
+
+  it('should see orders-service querying postgresql named postgres and inserting an order with uuid as id and integer price_in_cents', async () => {
+    expectTrace(traceloop.serviceByName('orders-service'))
+      .toQueryPostgreSQL()
+      .withDatabaseName('postgres', { compareType: COMPARE_TYPE.EQUALS })
+      .withStatement(
+        /INSERT INTO orders \(id, price_in_cents\) VALUES \('[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}', [0-9]+\)/,
+        { compareType: COMPARE_TYPE.REGEX },
+      );
+  });
+});
