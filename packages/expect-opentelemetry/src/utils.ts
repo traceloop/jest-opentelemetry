f5fdc2d2e69aea9e54e8916b94d431f9c0545f49
Tomer <4tomer.friedman@gmail.com>
2023-03-19 20:49:52 -0700

    print

--- packages/expect-opentelemetry/src/utils.ts
+++ packages/expect-opentelemetry/src/utils.ts
@@ -103,6 +103,9 @@ export function httpGetBinary(
         const result = Buffer.concat(data);
         resolve(result);
       });
+      res.on('error', function () {
+        console.log('error http get err listener');
+      });
     });
   });
 }
