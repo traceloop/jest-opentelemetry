c35dc7756e7b37715798f767a6a042230305d9b5
Tomer <42910634+tomer-friedman@users.noreply.github.com>
2023-06-13 18:25:33 +0300

    feat: separate tables and operations (#77)

--- packages/expect-opentelemetry/src/resources/postgresql-query.ts
+++ packages/expect-opentelemetry/src/resources/postgresql-query.ts
@@ -1,5 +1,6 @@
 import { SemanticAttributes } from '@opentelemetry/semantic-conventions';
 import { opentelemetry } from '@traceloop/otel-proto';
+import { Parser } from 'node-sql-parser';
 import {
   CompareOptions,
   filterByAttributeStringValue,
@@ -9,6 +10,7 @@ export class PostgreSQLQuery {
   constructor(
     readonly spans: opentelemetry.proto.trace.v1.ISpan[],
     private readonly serviceName: string,
+    private parser = new Parser(),
   ) {}
 
   withDatabaseName(name: string | RegExp, options?: CompareOptions) {
@@ -45,9 +47,7 @@ export class PostgreSQLQuery {
     return new PostgreSQLQuery(filteredSpans, this.serviceName);
   }
 
-  withOperationAndTable(operation: string, table: string) {
-    const regex = new RegExp(`${operation}.*${table}`, 'i'); // case insensitive
-
+  withOperations(...operations: string[]) {
     const filteredSpans = this.spans.filter((span) => {
       const statement = span.attributes?.find(
         (attribute) => attribute.key === SemanticAttributes.DB_STATEMENT,
@@ -57,15 +57,51 @@ export class PostgreSQLQuery {
         return false;
       }
 
-      return regex.test(statement);
+      const lowerCaseStatement = statement.toLowerCase();
+
+      return operations.every((operation) =>
+        lowerCaseStatement.includes(operation.toLowerCase()),
+      );
     });
 
     if (filteredSpans.length === 0) {
       throw new Error(
-        `No query by ${this.serviceName} to postgresql with operation ${operation} and table ${table} was found`,
+        `No query by ${this.serviceName} to postgresql with operations ${operations} was found`,
+      );
+    }
+
+    return new PostgreSQLQuery(filteredSpans, this.serviceName);
+  }
+
+  withTables(...tables: string[]) {
+    const filteredSpans = this.spans.filter((span) => {
+      const statement = span.attributes?.find(
+        (attribute) => attribute.key === SemanticAttributes.DB_STATEMENT,
+      )?.value?.stringValue;
+
+      if (!statement) {
+        return false;
+      }
+
+      const allTablesInStatement = this.parser
+        .tableList(prepareQuery(statement), { database: 'PostgresQL' })
+        .map((table) => table.split('::')[2].trim());
+
+      return tables.every((table) =>
+        allTablesInStatement.includes(table.toLowerCase()),
+      );
+    });
+
+    if (filteredSpans.length === 0) {
+      throw new Error(
+        `No query by ${this.serviceName} to postgresql with tables ${tables} was found`,
       );
     }
 
     return new PostgreSQLQuery(filteredSpans, this.serviceName);
   }
 }
+
+const prepareQuery = (
+  query: string, // remove double quotes and replace %s with 111
+) => query.replace(/"/g, '').replace(/%s/g, '111').toLocaleLowerCase();
